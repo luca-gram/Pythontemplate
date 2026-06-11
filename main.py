fc550a2d7186ec747934ef14e9288e16ac484ba7
luca-gram <112165@st-maartenscollege.nl>
2026-06-11 10:43:53 +0000

    fdghj

--- main.py
+++ main.py
@@ -12,23 +12,25 @@ print("Het woord heeft", len(Woord), "letters")
 while pogingen > 0:
    print("\nHuidige woord:", " ".join([letter if letter in geraden_letters else "_" for letter in Woord]))
    print("Geraden letters:", " ".join(sorted(geraden_letters)))
-   input("kies een letter: ")
-   break
-
-letter = input("Kies een letter: ").lower()
-
-  if letter in geraden_letters:
-   print("Je hebt deze letter al geraden. Probeer opnieuw.")
-continue
-   geraden_letters.add(letter)
-   if letter in Woord:
-      print("Goed geraden!")
-      if all(letter in geraden_letters for letter in Woord):
-         print("Gefeliciteerd! Je hebt het woord geraden:", Woord)
+   letter = input("Kies een letter: ").lower()
+   if letter in geraden_letters:
+      print("Je hebt deze letter al geraden. Probeer opnieuw.")
+      geraden_letters.add(letter)
+      if letter in Woord:
+         print("Goed geraden!")
+         geraden_letters.add(letter)
+         print()
+         if all(letter in geraden_letters for letter in Woord):
+            print("Gefeliciteerd! Je hebt het woord geraden:", Woord)
          else:
-         print("Fout! De letter is niet in het woord.")
-         pogingen -= 1
-         print("Je hebt nog", pogingen, "pogingen over.")
+            print("Fout! De letter is niet in het woord.")
+            pogingen -= 1
+            print("Je hebt nog", pogingen, "pogingen over.")
+      
+        
+
+
+
      
 
 
